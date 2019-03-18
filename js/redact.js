f946fc41e9b0a69a4e461acfdac151296f4e78a7
Olga <hloesilent@gmail.com>
2019-03-18 18:35:21 +0300

    1.4 done -close redactor by clicking on close button or typying esc button

--- js/redact.js
+++ js/redact.js
@@ -1,4 +1,5 @@
-;'use strict';
+;
+'use strict';
 // form.js — модуль, который ведет  работу с фотографиями, размещенными другими участниками
 
 
@@ -6,14 +7,31 @@
 
   window.redact = {};
 
-var uploadFileElement = document.querySelector('#upload-file');
+  var uploadFileElement = document.querySelector('#upload-file');
 
-var preview = document.querySelector('.img-upload__overlay');
+  var preview = document.querySelector('.img-upload__overlay');
+  var closeRedactorButtonElement = document.querySelector('#upload-cancel');
 
-var showRedactor = function() {
-  preview.classList.remove("hidden");
-}
 
-uploadFileElement.addEventListener("change", showRedactor);
+  var showRedactor = function () {
+    preview.classList.remove("hidden");
+
+  };
+
+  var hideRedactor = function () {
+    preview.classList.add("hidden");
+  };
+
+
+  if (preview.classList.contains("hidden")) { // закрытие редактора
+    closeRedactorButtonElement.addEventListener("click", hideRedactor); // по клику на крестик
+    document.addEventListener('keydown', function(evt) { // обработчик на весь документ нажатие клавиши
+      if (evt.keyCode === 27) { // если это клавиша ecs
+        hideRedactor(); // закрыть редактор
+      }
+    });
+  }
+
+  uploadFileElement.addEventListener("change", showRedactor);// показать редактор при загрузке img через fileuploader
 
 })();
