25719fa5c121925dbe8c7a7f6e55b2d2244fad40
Olga <hloesilent@gmail.com>
2019-03-19 15:46:32 +0300

    made function to resize an img

--- js/redact.js
+++ js/redact.js
@@ -26,29 +26,21 @@
     preview.classList.add("hidden");
   };
 
-  var scaleControllPlus = function () {
-    var num = parseInt(document.querySelector('.resize__control--value').value); //
+/*функция ресайзинга изображения */
+  var scaleControll = function (event) { // берет на вход событие
 
-    num += 25;
-    if (num <= 100) {
-      var scaleNumber = num / 100;
-      resizeControlElement.value = num + "%";
-      ImfUploadPreviewElement.style.transform = 'scale(' + scaleNumber + ')';
-      console.log("resizeControlElement.value + is " + resizeControlElement.value + " scaleNumber is " + scaleNumber);
+    var num = parseInt(document.querySelector('.resize__control--value').value); // считывает число, которое указано как value для инпута размера изображения
+
+    event.target.classList.contains("resize__control--plus") ? num += 25: num -= 25; // проверка класса элемента, по которому кликнули, если это класс плюс, то value увеличивается на 25, если нет(значит это класс минус, тк вешали обработчик только на них), то уменьшается на 25
+
+     if ( num > 0 && num <= 100) { // по ТЗ value может быть только от 0 до  100, поэтому мы будем его менять только в этом диапазоне
+      var scaleNumber = num / 100; // заносим в переменную дробное число равное num / 100, тк scale  у нас может быть ТОЛЬКО меньше 1
+      resizeControlElement.value = num + "%"; // меняем value инпута и добавляем туда %, тк там число со знаком %
+      ImfUploadPreviewElement.style.transform = 'scale(' + scaleNumber + ')'; // меняем размер картинки через свойство transform: scale
     }
+
   };
 
-  var scaleControllMinus = function () {
-    var num = parseInt(document.querySelector('.resize__control--value').value); //
-
-    num -= 25;
-    if (num > 0) {
-      var scaleNumber = num / 100;
-      resizeControlElement.value = num + "%";
-      ImfUploadPreviewElement.style.transform = 'scale(' + scaleNumber + ')';
-      console.log("resizeControlElement.value + is " + resizeControlElement.value + " scaleNumber is " + scaleNumber);
-    }
-  };
 
   // закрытие редактора
   closeRedactorButtonElement.addEventListener("click", hideRedactor); // по клику на крестик
@@ -64,8 +56,10 @@
 
   setRedactorNew();
 
-  plusResizeButton.addEventListener("click", scaleControllPlus);
-  minusResizeButton.addEventListener("click", scaleControllMinus);
+  // plusResizeButton.addEventListener("click", scaleControllPlus);
+  // minusResizeButton.addEventListener("click", scaleControllMinus);
+  plusResizeButton.addEventListener("click", scaleControll);
+  minusResizeButton.addEventListener("click", scaleControll);
 
 
 
